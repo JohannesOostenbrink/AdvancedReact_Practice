2bd677a9b35102db7acaf6b99cae8aaa13d7000a
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-19 16:14:36 +0700

    Practice Rendering List in React

---
+++ src/DessertsList.js
@@ -0,0 +1,53 @@
+// display a list of desserts with less than 500 calories, all sorted by calories, from low to high.
+
+// const DessertsList = (props) => {
+//     const LowCaloriesDesserts = props.data
+//       .filter((dessert) => {
+//         return dessert.calories < 500;
+//       })
+//       .sort((a, b) => {
+//         return a.calories - b.calories;
+//       })
+//       .map((dessert) => {
+//         return (
+//           <li>
+//             {dessert.name} - {dessert.calories} cal
+//           </li>
+//         )
+//       }
+      
+//     )
+//     return <ul>{LowCaloriesDesserts}</ul>; 
+  
+//   }
+  
+  
+  
+  // function DessertsList(props) {
+    
+  //   return (
+  
+  //   const lowCaloriesDesserts = props.data
+        
+  //   .filter((dessert) => {
+  //   return dessert.calories < 500;
+  //   })
+  
+  //   .sort((a, b) => { 
+  //    return a.calories - b.calories; 
+  //   })
+      
+//   .map((dessert) => { 
+//    return ( 
+//     <li> 
+//        {dessert.name} - {dessert.calories} cal 
+//      </li> 
+//    ); 
+
+// }
+
+// return <ul>{lowCaloriesDesserts}</ul>; 
+
+
+
+// export default DessertsList;
