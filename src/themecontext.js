ad31e33506f06b0bc0d813a73e291fb03b590f04
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-29 08:35:46 +0700

    ThemeContext using React.CreateContext

--- src/themecontext.js
+++ src/themecontext.js
@@ -1,3 +1,11 @@
+import React from 'react'; 
+
+const ThemeContext = React.createContext();
+
+export default ThemeContext;
+
+
 export const ThemeProvider = ({ children }) => children;
 
 export const useTheme = () => ({ theme: "light" });
+
