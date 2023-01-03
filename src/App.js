7a2e894cbd0fc0db074292429314247cf3e5abbc
JohannesOostenbrink <oostenhan@gmail.com>
2023-01-03 09:46:56 +0700

    Initial setup for homework

--- src/App.js
+++ src/App.js
@@ -165,88 +165,131 @@
 
 // export default App;
 
-import "./App.css";
-import { ThemeProvider, useTheme } from "./ThemeContext";
-import Switch from "./Switch";
+// import "./App.css";
+// import { ThemeProvider, useTheme } from "./ThemeContext";
+// import Switch from "./Switch";
 
-const Title = ({ children }) => {
-  const { theme } = useTheme();
-  return (
-    <h2
-      style={{
-        color: theme === "light" ? "black" : "white",
-      }}
-    >
-      {children}
-    </h2>
-  );
-};
+// const Title = ({ children }) => {
+//   const { theme } = useTheme();
+//   return (
+//     <h2
+//       style={{
+//         color: theme === "light" ? "black" : "white",
+//       }}
+//     >
+//       {children}
+//     </h2>
+//   );
+// };
 
-const Paragraph = ({ children }) => {
-  const { theme } = useTheme();
-  return (
-    <p
-      style={{
-        color: theme === "light" ? "black" : "white",
-      }}
-    >
-      {children}
-    </p>
-  );
+// const Paragraph = ({ children }) => {
+//   const { theme } = useTheme();
+//   return (
+//     <p
+//       style={{
+//         color: theme === "light" ? "black" : "white",
+//       }}
+//     >
+//       {children}
+//     </p>
+//   );
+//     }
+
+//   const Content = () => {
+//     return (
+//       <div>
+//         <Paragraph>
+//           We are a pizza loving family. And for years, I searched and searched and
+//           searched for the perfect pizza dough recipe. I tried dozens, or more.
+//           And while some were good, none of them were that recipe that would
+//           make me stop trying all of the others.
+//         </Paragraph>
+//       </div>
+//     );
+//   };
+  
+//   const Header = () => {
+//     return (
+//       <header>
+//         <Title>Little Lemon 🍕</Title>
+//         <Switch />
+//       </header>
+//     );
+//   };
+  
+//   const Page = () => {
+//     return (
+//       <div className="Page">
+//         <Title>When it comes to dough</Title>
+//         <Content />
+//       </div>
+//     );
+//   };
+
+//   function App() {
+//     const { theme } = useTheme();
+//     return (
+//       <div
+//         className="App"
+//         style={{
+//           backgroundColor: theme === "light" ? "white" : "black",
+//         }}
+//       >
+//         <Header />
+//         <Page />
+//       </div>
+//     );
+//   }
+  
+//   function Root() {
+//     return (
+//       <ThemeProvider>
+//         <App />
+//       </ThemeProvider>
+//     );
+//   }
+  
+//   export default Root;
+
+import { useState } from "react";
+
+export default function App() {
+  const [giftCard, setGiftCard] = useState(
+    {
+        firstName: "Jennifer",
+        lastName: "Smith",
+        text: "Free dinner for 4 guests",
+        valid: true,
+        instructions: "To use your coupon, click the button below.",
     }
+  );
 
-  const Content = () => {
-    return (
-      <div>
-        <Paragraph>
-          We are a pizza loving family. And for years, I searched and searched and
-          searched for the perfect pizza dough recipe. I tried dozens, or more.
-          And while some were good, none of them were that recipe that would
-          make me stop trying all of the others.
-        </Paragraph>
-      </div>
-    );
-  };
-  
-  const Header = () => {
-    return (
-      <header>
-        <Title>Little Lemon 🍕</Title>
-        <Switch />
-      </header>
-    );
-  };
-  
-  const Page = () => {
-    return (
-      <div className="Page">
-        <Title>When it comes to dough</Title>
-        <Content />
-      </div>
-    );
-  };
+  function spendGiftCard() {
 
-  function App() {
-    const { theme } = useTheme();
-    return (
-      <div
-        className="App"
-        style={{
-          backgroundColor: theme === "light" ? "white" : "black",
-        }}
-      >
-        <Header />
-        <Page />
-      </div>
-    );
   }
-  
-  function Root() {
-    return (
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    );
-  }
-  
-  export default Root;
+
+  return (
+    <div style={{padding: '40px'}}>
+      <h1>
+        Gift Card Page
+      </h1>
+      <h2>
+        Customer: {giftCard.firstName} {giftCard.lastName}
+      </h2>
+      <h3>
+        {giftCard.text}
+      </h3>
+      <p>
+        {giftCard.instructions}
+      </p>
+      {
+        giftCard.valid && (
+          <button onClick={spendGiftCard}>
+            Spend Gift Card
+          </button>
+                  )
+                }
+              </div>
+            );
+          }
+          
