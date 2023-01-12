4c1be599edf1f96a10159a6dbeca264a283fef5a
JohannesOostenbrink <oostenhan@gmail.com>
2023-01-12 13:57:29 +0700

    Practice React APIs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+//Practice react.cloneElement and React.map.children
+
+// import "./App.css";
+// import { RadioGroup, RadioOption } from "./Radio";
+// import { useState } from "react";
+
+// function App() {
+//   const [selected, setSelected] = useState("");
+//   return (
+//     <div className="App">
+//       <h2>How did you hear about Little Lemon?</h2>
+//       <RadioGroup onChange={setSelected} selected={selected}>
+//         <RadioOption value="social_media">Social Media</RadioOption>
+//         <RadioOption value="friends">Friends</RadioOption>
+//         <RadioOption value="advertising">Advertising</RadioOption>
+//         <RadioOption value="other">Other</RadioOption>
+//       </RadioGroup>
+//       <button disabled={!selected}>Submit</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 //Create Custom Hook
 
 // import { useState, useEffect, useRef } from "react";
